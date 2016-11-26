a2b6bf380e68811ef2344c1d392df250f34ae378
talk2 <talk2@10.0.0.186>
2016-11-25 23:02:24 -0500

    CsvReader Commit: > Added Test for instantiating a CsvReader object > Added a constructor for the CsvReader class > Added required fields for the CsvReader class

--- ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/CsvReader.java
+++ ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/CsvReader.java
@@ -1,9 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Written by WCUPA Computer Science club members 
+ * November 2016
  */
 package edu.wcupa.cscclub.projects.martinbrower.reportgenerator.model;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,5 +15,15 @@ package edu.wcupa.cscclub.projects.martinbrower.reportgenerator.model;
  */
 public class CsvReader
 {
+    private ArrayList<Page> _pages;
+    private int _pageCount;
+    private BufferedReader _reader;
+
+    public CsvReader(String fileName) throws FileNotFoundException
+    {
+        FileReader reader = new FileReader(fileName);
+        _reader = new BufferedReader(reader);
+        _pages = new ArrayList<>();
+    }
     
 }
---
+++ ReportGenerator/test/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/When_Testing_CsvReader_With_Existing_File.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wcupa.cscclub.projects.martinbrower.reportgenerator.model;
+
+import java.io.FileNotFoundException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author talk2
+ */
+public class When_Testing_CsvReader_With_Existing_File
+{   
+    private final String _fileName;
+    
+    public When_Testing_CsvReader_With_Existing_File() {
+        _fileName = "C:\\Users\\talk2\\Desktop\\Files\\Report1.csv";
+    }
+    
+    @Test
+    public void instance_should_be_created()
+    {        
+        try
+        {
+            CsvReader reader = new CsvReader(_fileName);            
+            assertTrue(true); // Instance created; therefore, test passed!
+        }
+        catch (FileNotFoundException ex)
+        {
+            // TODO: Log failure somewhere
+            assertTrue(ex.getMessage(), false); // Test failed!
+        }
+    }
+    
+}
