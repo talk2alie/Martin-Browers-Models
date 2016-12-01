720bcc6f8bb54a96905119031c61c240fc3f9bef
talk2 <talk2@MalieTech-1.wireless.wcupa.net>
2016-11-30 21:24:59 -0500

    > Modified code to secure a page's data from unwanted edits

--- ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/Page.java
+++ ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/Page.java
@@ -5,7 +5,6 @@
 package edu.wcupa.cscclub.projects.martinbrower.reportgenerator.model;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,70 +15,31 @@ import java.util.Map;
  * report
  */
 public class Page
-{
-    private int _number;
-    private int _cartTotal;    
-    private final ArrayList<Column> _columns;
+{   
+    /**
+     * Gets the wanted columns on the page
+     */
+    public final ArrayList<Column> COLUMNS;
     
     /**
-     * The summary rows on this page
+     * Gets the summary rows on the page
      */
     public final Map<String, String> SUMMARIES;
     
     /**
-     * Creates a new page in a report or in a parsed CSV file
+     * Gets the cardinal position of this 
+     * page in the CSV file or report
      */
-    public Page() {
+    public final int NUMBER;
+    
+    /**
+     * Creates a new page in a report or in a 
+     * parsed CSV file, given the page number
+     * @param number The page number
+     */
+    public Page(int number) {
+        NUMBER = number;
         SUMMARIES = new HashMap<>();
-        _columns = new ArrayList<>();
+        COLUMNS = new ArrayList<>();
     }
-    
-    /**
-     * Gets the CART TOTAL as read from the CSV file
-     * @return The CART TOTAL as read from the CSV file
-     */
-    public int getCartTotal()
-    {
-        return _cartTotal;
-    }
-
-    /**
-     * Sets the CART TOTAL as read from the CSV file
-     * @param cartTotal The CART TOTAL as read from the CSV file
-     */
-    public void setCartTotal(int cartTotal)
-    {
-        this._cartTotal = cartTotal;
-    }
-    
-    /**
-     * Gets all required columns based on the specified needed columns
-     * @return The required columns from the CSV file, sorted in 
-     * ascending order of their header cells
-     */
-    public ArrayList<Column> getColumns()
-    {
-        Collections.sort(_columns);
-        return _columns;
-    }
-
-    /**
-     * Gets the cardinal number of this page. If this is the last page in 
-     * a CSV file, this value represents the number of pages in that CSV 
-     * file
-     * @return The cardinal number of this page
-     */
-    public int getNumber()
-    {
-        return _number;
-    }
-
-    /**
-     * Sets the cardinal number of this page
-     * @param number The cardinal number of this page
-     */
-    public void setNumber(int number)
-    {
-        this._number = number;
-    }    
 }
